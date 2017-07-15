6c7e227d9521f1ab1221a8cc59b551b7ea3148e2
kunlaotou <tangwenhua2010@126.com>
2017-07-15 15:08:49 +0800

    Create range_arange.py

---
+++ range_arange.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 15 10:03:22 2017
+
+@author: tangwenhua
+"""
+
+import numpy as np
+"""
+range(start, end, step)，返回一个list对象，起始值为start，终止值为end，但不含终止值，步长为step。只能创建int型list。
+arange(start, end, step)，与range()类似，但是返回一个array对象。需要引入import numpy as np，并且arange可以使用float型数据。
+"""
+
+a =[ i for i in range(1,10,2)]
+print(a) #[1, 3, 5, 7, 9]
+
+b =[np.arange(1,10,2)]
+print(b)
